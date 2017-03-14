3e16ef4441f0642981b676befe0e3841a9fd36d2
frankw <fwessels@xs4all.nl>
2017-03-13 18:38:12 -0700

    Add testing code

--- test/MultiplyAndAdd_amd64.go
+++ test/MultiplyAndAdd_amd64.go
@@ -5,18 +5,30 @@ package c2goasmtest
 
 import (
 	"unsafe"
+	"fmt"
 )
 
 //go:noescape
 func _MultiplyAndAdd(vec1, vec2, vec3, result unsafe.Pointer)
 
+type drijvend struct {
+	a,b,c,d,e,f,g,h float32
+}
+
 func MultiplyAndAdd(f1, f2, f3 float64) float64 {
 
-	_f1 := f1
-	_f2 := f2
-	_f3 := f3
-	_f4 := float64(0.0)
+	_f1 := drijvend{}
+	_f2 := drijvend{}
+	_f3 := drijvend{}
+	_f4 := drijvend{}
+
+	_f1.b = 1.0
+	_f2.b = 2.0
+	_f3.b = 3.0
+
 	_MultiplyAndAdd(unsafe.Pointer(&_f1), unsafe.Pointer(&_f2), unsafe.Pointer(&_f3), unsafe.Pointer(&_f4))
 
-	return _f4
+	fmt.Println(_f4.b)
+
+	return 0.0
 }

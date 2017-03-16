cfe2c944768b498511976de8a911320c92b00d40
frankw <fwessels@xs4all.nl>
2017-03-16 16:06:34 -0700

    Update test case for memcpy()

--- test/MaddMemcpy_amd64.go
+++ test/MaddMemcpy_amd64.go
@@ -8,13 +8,13 @@ import (
 )
 
 //go:noescape
-func _MaddMemcpy(vec1, vec2, vec3, result unsafe.Pointer)
+func _MaddMemcpy(vec1, vec2, vec3 unsafe.Pointer, size1, size2 uint64, result unsafe.Pointer)
 
-func MaddMemcpy(f1, f2, f3 [8]float32) [8]float32 {
+func MaddMemcpy(f1, f2, f3 *[8]float32, size1, size2 uint64) [8]float32 {
 
 	_f4 := [8]float32{}
 
-	_MaddMemcpy(unsafe.Pointer(&f1), unsafe.Pointer(&f2), unsafe.Pointer(&f3), unsafe.Pointer(&_f4))
+	_MaddMemcpy(unsafe.Pointer(f1), unsafe.Pointer(f2), unsafe.Pointer(f3), size1, size2, unsafe.Pointer(&_f4))
 
 	return _f4
 }
--- test/MaddMemcpy_test.go
+++ test/MaddMemcpy_test.go
@@ -16,10 +16,17 @@ func TestMaddMemcpy(t *testing.T) {
 		f3[i] = float32(i * 3)
 	}
 
-	f4 := MaddMemcpy(f1, f2, f3)
+	f4 := MaddMemcpy(&f1, &f2, &f3, 6*4, 7*4)
 
 	for i := 0; i < 8; i++ {
-		expected := f1[i]*f2[i]+f3[i]
+		expected := float64(0)
+		if i < 6 {
+			expected = f1[i]*f1[i]+f1[i]
+		} else if i < 7 {
+			expected = f1[i]*f1[i]+f3[i]
+		} else {
+			expected = f1[i]*f2[i]+f3[i]
+		}
 		if f4[i] != expected {
 			t.Errorf("TestMaddMemcpy(): \nexpected %f\ngot      %f", expected, f4[i])
 		}

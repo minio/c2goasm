b88b26e4439948d9558ac925d3ca06ad5c5100c5
frankw <fwessels@xs4all.nl>
2017-03-26 21:13:47 -0700

    Better and more robust detection of subroutines

--- segment.go
+++ segment.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"strings"
+	"regexp"
 )
+
+var regexpRet = regexp.MustCompile(`^\s*ret`)
 
 type Segment struct {
 	Name       string
@@ -40,98 +43,45 @@ func segmentEqual(a, b []Segment) bool {
 	return true
 }
 
+// Segment the soure into multiple routines
 func SegmentSource(src []string) []Segment {
 
-	exits := []Exit{}
+	globals := SplitOnGlobals(src)
 
-	exitGatherUntilRet := -1
-
-	// Collect exit of subroutines
-	for index, line := range src {
-
-		if strings.HasSuffix(line, ".exit") {
-			exitName := ExtractName(strings.Split(line, "## %")[1])
-
-			for _, e := range exits {
-				if e.Name == exitName {
-					panic(fmt.Sprintf("Exit name %s already found", exitName))
-				}
-			}
-
-			exits = append(exits, Exit{Name: exitName, End: index + 1})
-
-			// Gather stack information
-			exitGatherUntilRet = len(exits) - 1
-		}
-
-		if exitGatherUntilRet != -1 && strings.Contains(line, "ret") {
-
-			// Lines of postamble
-			stackLines := src[exits[exitGatherUntilRet].End : index+1]
-
-			exits[exitGatherUntilRet].stack = ExtractStackInfo(stackLines)
-
-			exitGatherUntilRet = -1
-		}
+	if len(globals) == 0 {
+		return []Segment{}
 	}
 
 	segments := []Segment{}
 
-	entryGatherUntilRet := -1
 
-	instructions := true
-
-	// Find start of subroutines
-	for index, line := range src {
-
-		if strings.Contains(line, ".section") {
-			instructions = strings.Contains(line, "pure_instructions")
+	for splitBegin, g := globals[0], 0; g < len(globals); g++ {
+		splitEnd := len(src)
+		if g < len(globals) - 1 {
+			splitEnd = globals[g + 1]
 		}
 
-		if instructions && strings.Contains(line, "## @") {
-			entryName := ExtractName(strings.Split(line, "## @")[1])
+		scrambledName := strings.TrimSpace(strings.Split(src[splitBegin], ".globl")[1])
+		entryName := ExtractName(scrambledName)
 
-			for _, s := range segments {
-				if s.Name == entryName {
-					panic(fmt.Sprintf("Entry name %s already found", entryName))
+		// Search for ret statement from the back
+		for index := splitEnd-1; index >= splitBegin; index-- {
+			if match := regexpRet.FindStringSubmatch(src[index]); len(match) > 0 {
+
+				// Found closing ret statement, start searching back to first non closing statement
+				i := 1
+				for ; index-i >= 0; i++ {
+					if !IsEpilogueInstruction(src[index-i]) {
+						break
+					}
 				}
-			}
 
-			var stack Stack
-			end := -1
-			for _, e := range exits {
-				if e.Name == entryName {
-					end = e.End
-					stack = e.stack
-					break
-				}
-			}
-
-			segments = append(segments, Segment{Name: entryName, Start: index + 1, End: end, stack: stack})
-
-			if end == -1 {
-				// No exit label found, start searching for ret statement
-				entryGatherUntilRet = len(segments) - 1
+				startLine := splitBegin + findLabel(src[splitBegin:splitEnd], scrambledName) + 1
+				segments = append(segments, Segment{Name: entryName, Start: startLine, End: index - i + 1})
 			}
 		}
 
-		if entryGatherUntilRet != -1 && strings.Contains(line, "ret") {
-
-			// Found closing ret statement, start searching back to first non closing statement
-			i := 1
-			for ; index-i >= 0; i++ {
-				if !IsEpilogueInstruction(src[index-i]) {
-					break
-				}
-			}
-			segments[entryGatherUntilRet].End = index - i + 1
-
-			stackLines := src[index-i+1 : index+1]
-
-			segments[entryGatherUntilRet].stack = ExtractStackInfo(stackLines)
-
-			entryGatherUntilRet = -1
-		}
+		splitBegin = splitEnd
 	}
 
 	return segments
@@ -157,4 +107,17 @@ func SegmentEatPrologue(lines []string, stack *Stack) int {
 	}
 
 	return index
+}
+
+func findLabel(lines []string, label string) int {
+
+	labelDef := label + ":"
+
+	for index, line := range lines {
+		if strings.HasPrefix(line, labelDef) {
+			return index
+		}
+	}
+
+	panic(fmt.Sprintf("Failed to find label: %s", labelDef))
 }
